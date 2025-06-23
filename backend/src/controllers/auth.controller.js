0dad26ab43bb8bd97f38758fc75a0432188d0263
Gosia Magdzik <melassa26@gmail.com>
2025-06-23 18:09:29 +0200

    controllers file & routes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const signup = (req, res) => {
+  res.send("signup route");
+};
+
+export const login = (req, res) => {
+  res.send("login route");
+};
+
+export const logout = (req, res) => {
+  res.send("logout route droga");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -8,8 +8,3 @@ app.use("/api/auth", authRoutes);
 app.listen(5000, () => {
   console.log("server Started at 5000");
 });
-
-// router.get("/", getProducts);
-// router.post("/", createProduct);
-// router.put("/:id", updateProduct); //PATH , ALTERNATYWA (SOME METHODS)
-// router.delete("/:id", deleteProduct);
