3373b4886a6daa16dd03cc861b8e0c69336bacd5
Gosia Magdzik <melassa26@gmail.com>
2025-06-24 18:38:35 +0200

    typo

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -17,7 +17,7 @@ export const signup = async (req, res) => {
     const user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "Email already exists" });
 
-    const salt = await bcrypt.getSalt(10);
+    const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
