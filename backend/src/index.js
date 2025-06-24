aecd8bbc8d23ef1df43efcdaa5a6da29c74f96a2
Gosia Magdzik <melassa26@gmail.com>
2025-06-24 15:30:35 +0200

    data base setUp - mongoDB + .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("server Started at 5000");
+app.listen(PORT, () => {
+  console.log("server Started at " + PORT);
 });
