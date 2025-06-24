447f0248edc9a8c6cca119ae610d2077e088e93a
Gosia Magdzik <melassa26@gmail.com>
2025-06-24 20:07:42 +0200

    react router implemented & Navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-red-500"> Chat App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
