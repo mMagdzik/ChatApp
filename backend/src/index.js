f49934d80e6017360cfb916ea642b87988f00a6d
Gosia Magdzik <melassa26@gmail.com>
2025-06-24 15:48:23 +0200

    connectDB()

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -10,5 +11,6 @@ const app = express();
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log("server Started at " + PORT);
 });
