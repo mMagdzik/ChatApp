1eacd435daf48385e944a28701ce18825c523cab
Gosia Magdzik <melassa26@gmail.com>
2025-06-25 15:41:53 +0200

    endpoint for updating the profile page

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,9 +4,9 @@ import { signup, login, logout } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
+
+router.put("/update-profile", updateProfile);
 
 export default router;
