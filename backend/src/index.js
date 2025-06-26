7b0a4f5342a969ed161949f71b76788be80f8207
Gosia Magdzik <melassa26@gmail.com>
2025-06-26 15:56:42 +0200

    message - modal and endpoint.

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/message", authRoutes);
 
 app.listen(PORT, () => {
   console.log("server Started at " + PORT);
