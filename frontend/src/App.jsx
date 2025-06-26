04782a127bdb1bb6f0b300f91acb98eb6ed29362
Gosia Magdzik <melassa26@gmail.com>
2025-06-26 15:29:58 +0200

    frontend -> building a main pages structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </>
   );
 };
