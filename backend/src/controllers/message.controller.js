bed2cb6390b703b84fccbcd55ee3f99e766b47c4
Gosia Magdzik <melassa26@gmail.com>
2025-06-26 19:39:04 +0200

    error fixed

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,4 @@
-import Message from "../models/message.model";
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
+  console.log({ authUser });
 
   return (
     <>
